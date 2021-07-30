76bd7812b2f692d0bb4c41c7f0d2a6a704e00667
deletd <45876429+deletd@users.noreply.github.com>
2021-07-30 10:58:56 +0300

    GNL
    Get_next_line function is a tool that helps you to read from file descriptor line by line.

---
+++ GNL/get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_find_endl(char *tmp);
+char	*ft_init(char *rem);
+char	*ft_cut(char *rem);
+int		ft_strlen(char *s);
+void	*ft_calloc(size_t num, size_t size);
+void	*ft_memset(void *dest, int c, size_t n);
+int		ft_count_to_endl(char *s);
+int		ft_free(char *tmp);
+
+#endif
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
